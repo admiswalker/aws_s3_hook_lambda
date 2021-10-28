ca7eb57121b2c88fc304d62c4cb7780d088303e7
admiswalker <admiswalker@gmail.com>
2021-10-28 23:22:19 +0900

    functionalize the s3 bucket reading

--- infra/lib/lambda/cnvFile/index.py
+++ infra/lib/lambda/cnvFile/index.py
@@ -2,10 +2,29 @@ import json
 import urllib.parse
 import boto3
 
-print('Loading function')
+def get_hookingBucketNameAndKey(event):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    return bucket, key
 
-s3 = boto3.client('s3')
+def readObject(bucket, key='out.txt'):
+    s3 = boto3.client('s3')
+    print('bucket: %s' % bucket)
+    print('key: %s' % key)
+    
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
 
+    # httpStatusCode = response['ResponseMetadata']['HTTPStatusCode']
+    # lastModified = response['LastModified']
+    # contentType = response['ContentType']
+    size = response['ContentLength']
+    body = response['Body'].read()
+    return size, body
 '''
 def writeObject(bucket, key='out.txt'):
     obj = bucket.Object(fname)
@@ -13,37 +32,17 @@ def writeObject(bucket, key='out.txt'):
     obj.put( Body='Hello AWS S3!' )
     return
 '''
-
 def handler(event, context):
-    print('in index handler')
-    print(event)
-    print(context)
-    
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    print('bucket: %s' % bucket)
-    print('key: %s' % key)
-    
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
-
-    # Read Object
+    hooking_bucket, hooking_key = get_hookingBucketNameAndKey(event)
+    size, body = readObject(hooking_bucket, hooking_key)
     print('--- printing object: begin ---')
-    print('response[\'ContentType\']:', response['ContentType'])
-    body = response['Body'].read()
-    print('response:', body)
+    print('response:', size, body)
     print('--- printing object: end ---')
     
-    # Write Object
 #    writeObject(bucket, key='out.txt')
     
     return {
-        "bucket: ": bucket,
-        "key: ": key
+        "bucket: ": hooking_bucket,
+        "key: ": hooking_key
     };
 
