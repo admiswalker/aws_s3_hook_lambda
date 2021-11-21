cb90fbecb2b746876d1f15537fa951ecf639a652
admiswalker <admiswalker@gmail.com>
2021-11-21 13:43:21 +0900

    mv files

--- lib/lambda/cnvFile/build_dockerfile.sh
+++ lib/lambda/cnvFile/build_dockerfile.sh
@@ -3,6 +3,6 @@ CONTAINER_NAME=gen-deployment-package
 GEN_TARGET=deployment-package.zip
 
 docker build -t $CONTAINER_NAME ./
-sh ./docker_cp.sh $CONTAINER_NAME ./home/$GEN_TARGET .
-sh ./docker_rmi.sh $CONTAINER_NAME
+sh ./docker_sh/cp.sh $CONTAINER_NAME ./home/$GEN_TARGET .
+sh ./docker_sh/rmi.sh $CONTAINER_NAME
 
