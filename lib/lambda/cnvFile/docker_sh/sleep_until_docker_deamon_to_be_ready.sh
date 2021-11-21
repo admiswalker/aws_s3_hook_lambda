300a76b97b15f74c2b0953cfd3259dd7c122bef1
admiswalker <admiswalker@gmail.com>
2021-11-21 16:36:51 +0900

    wip

--- lib/lambda/cnvFile/docker_sh/sleep_until_docker_deamon_to_be_ready.sh
+++ lib/lambda/cnvFile/docker_sh/sleep_until_docker_deamon_to_be_ready.sh
@@ -5,18 +5,25 @@
 #   ./docker_sh/sleep_until_server_starts.sh $CONTAINER_NAME
 
 CONTAINER_NAME=$1
+TMP_DIR=tmp_for_check_the_docker_deamon_running
+
+mkdir -p ./$TMP_DIR
+echo 'FROM scratch' >> ./$TMP_DIR/Dockerfile
 
 while :
 do
     echo 'waiting for docker daemon to be ready...'
 
-    str=$(docker exec -it $CONTAINER_NAME docker build ./docker_sh/for_check_the_docker_deamon_running)
+    str=$(docker exec -it $CONTAINER_NAME docker build ./$TMP_DIR)
     str_len=${#str}
     if [ $str_len == 0 ]; then
-	sleep 1
+	sleep 0.5
     elif [[ $str =~ 'not connect to the Docker daemon' ]]; then
-	sleep 1
+	sleep 0.5
     else
-        exit 0
+        break
     fi
 done
+
+rm -rf ./$TMP_DIR
+exit 0
